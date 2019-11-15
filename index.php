f95cc9c419d66cd7352cdb940c3b24b2b09d3bc1
Fülöp Dániel <fulopdaniel@gmail.com>
2019-11-15 18:34:59 +0100

    üdvözlés hozzáadva

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
         if (isset($_SESSION['user']) && (!empty($_SESSION['user']))){
             //be vagyok lépve
             echo file_get_contents("html/menu_in.html");
+            //var_dump($_SESSION['user']);
+            echo "<br><h2>Üdvözöllek kedves " . $_SESSION['user'][1] . "</h2>";
         }else{
             //nincs belépve senki
             echo file_get_contents("html/menu_out.html");
