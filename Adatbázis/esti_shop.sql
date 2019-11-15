b9613991ee550e0af916cbe7665f9a38d29b1b43
Fülöp Dániel <fulopdaniel@gmail.com>
2019-11-15 18:15:10 +0100

    alakulk

---
+++ Adatbázis/esti_shop.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2019. Nov 15. 17:58
+-- Kiszolgáló verziója: 10.1.34-MariaDB
+-- PHP verzió: 7.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `esti_shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `email` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
+  `active` tinyint(4) DEFAULT NULL,
+  `rights` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`ID`, `email`, `password`, `name`, `address`, `active`, `rights`) VALUES
+(1, 'user@vasvari.hu', '123456', 'Teszt Elek', '9000 Teszt Város, Elek utca 1.', 1, NULL),
+(3, 'a@a.hu', '123456', 'Kiss Béla', '6000 Nem tudom hol ', 1, NULL);
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ login.php
@@ -0,0 +1,40 @@
+<?php
+    session_start();
+    require_once('config/connect.php');
+    if (isset($_POST['email'])){
+        $email = $_POST['email'];
+        $pass = $_POST['passw'];
+        $sql = "SELECT * FROM users WHERE email = '".$email."' AND password = '".$pass."';";
+        $res = $conn -> query($sql);
+        if ($res && $res -> num_rows == 1) {
+            echo 'Sikeres a bejelentkezés';
+            $row = $res -> fetch_assoc();
+            print_r($row);
+            $_SESSION['user'] = array($row['ID'], $row['name']);
+            if (isset($_SESSION['error'])){
+                unset($_SESSION['error']);
+            }
+            
+        }else{
+            echo 'Sikertelen a bejelentkezés';
+            $_SESSION['error'] = "Helytelen belépési adatok";
+            header("Location: login.php");
+        }
+    }
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo file_get_contents("html/login_form.html");
+        if (isset($_SESSION['error'])){
+            echo "<h2>{$_SESSION['error']}</h2>";
+        }
+        ?>
+    </body>
+</html>
+
