fc8e11c6322f800ad01c4d50440ffe2231e23bf0
Fülöp Dániel <fulopdaniel@gmail.com>
2019-11-18 17:14:55 +0100

    Regisztráció működik

--- Adatbázis/esti_shop.sql
+++ Adatbázis/esti_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2019. Nov 15. 17:58
+-- Létrehozás ideje: 2019. Nov 18. 17:14
 -- Kiszolgáló verziója: 10.1.34-MariaDB
 -- PHP verzió: 7.2.8
 
@@ -34,6 +34,7 @@ CREATE TABLE `users` (
   `password` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
   `name` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
   `address` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
+  `regdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `active` tinyint(4) DEFAULT NULL,
   `rights` tinyint(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
@@ -42,9 +43,13 @@ CREATE TABLE `users` (
 -- A tábla adatainak kiíratása `users`
 --
 
-INSERT INTO `users` (`ID`, `email`, `password`, `name`, `address`, `active`, `rights`) VALUES
-(1, 'user@vasvari.hu', '123456', 'Teszt Elek', '9000 Teszt Város, Elek utca 1.', 1, NULL),
-(3, 'a@a.hu', '123456', 'Kiss Béla', '6000 Nem tudom hol ', 1, NULL);
+INSERT INTO `users` (`ID`, `email`, `password`, `name`, `address`, `regdate`, `active`, `rights`) VALUES
+(1, 'user@vasvari.hu', '123456', 'Teszt Elek', '9000 Teszt Város, Elek utca 1.', '0000-00-00 00:00:00', 1, NULL),
+(3, 'a@a.hu', '123456', 'Kiss Béla', '6000 Nem tudom hol ', '0000-00-00 00:00:00', 1, NULL),
+(4, 'b@b.hu', '123456', 'Új Emberke', 'Ő is valahol dolgozik', '2019-11-18 16:15:24', NULL, NULL),
+(5, 'asd@asd.hu', '123456', 'Teszt Elek', 'Elköltözött', '2019-11-18 16:59:58', NULL, NULL),
+(6, 'dani@gmail.com', 'dani', 'Dani', 'isisi', '2019-11-18 17:04:08', NULL, NULL),
+(12, 'asds@gggg.com', '123', 'Dani', '3333', '2019-11-18 17:12:56', NULL, NULL);
 
 --
 -- Indexek a kiírt táblákhoz
@@ -54,7 +59,8 @@ INSERT INTO `users` (`ID`, `email`, `password`, `name`, `address`, `active`, `ri
 -- A tábla indexei `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- A kiírt táblák AUTO_INCREMENT értéke
@@ -64,7 +70,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT a táblához `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- reg.php
+++ reg.php
@@ -1,7 +1,36 @@
 <?php
-
+    session_start();
+    require_once 'config/connect.php';
+    if ($_SERVER['REQUEST_METHOD'] == "POST"){
+        if(!empty($_POST['email'])){
+            $email = $_POST['email'];
+            $pwd = $_POST['passw'];
+            $name = $_POST['name'];
+            $address = $_POST['address'];            
+            
+            $stmt = $conn -> prepare("INSERT INTO users (email, password, name, address) VALUES (?, ?, ?, ?)");
+            $stmt -> bind_param("ssss", $email, $pwd , $name, $address);            
+            
+            if ($stmt -> execute()) {
+                header("Location: index.php");
+            } else {
+                $_SESSION['error'] = "Helytelen adatok";                        
+            }
+            
+            
+            $stmt -> close();
+            $conn -> close();
+            
+            
+        }        
+    }
+        
     echo file_get_contents("html/header.html");
-    echo "<h2>Itt lesz a regisztávió</h2>";
+    echo file_get_contents("html/reg_form.html");
+    if (!empty($_SESSION['error'])){
+        echo "<h2>".$_SESSION['error']."</h2>";
+        unset($_SESSION['error']);
+    }
     echo file_get_contents("html/footer.html");
-    
+        
 ?>
